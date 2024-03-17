10837876520e70758fd2268e4a32d12b1554d603
Onco <adrianonco@users.noreply.github.com>
2024-03-17 12:54:32 +0100

    Use static access with "javax.swing.SwingConstants" for "CENTER", "LEFT"

--- CarsProjectAlbert/src/CarsProjectDev/CarSalesSystem.java
+++ CarsProjectAlbert/src/CarsProjectDev/CarSalesSystem.java
@@ -85,9 +85,9 @@ public class CarSalesSystem extends JFrame implements ActionListener, ComponentL
 	private JPanel titlePanel = new JPanel(new GridLayout(2, 1));
 	private JLabel statusLabel = new JLabel();
 	private JLabel pictureLabel = new JLabel();
-	private JLabel carCoLabel = new JLabel("My Car Company", JLabel.CENTER);
-	private JLabel salesSysLabel = new JLabel("Car Sales System", JLabel.CENTER);
-	private JTabbedPane theTab = new JTabbedPane(JTabbedPane.LEFT);
+	private JLabel carCoLabel = new JLabel("My Car Company", SwingConstants.CENTER);
+	private JLabel salesSysLabel = new JLabel("Car Sales System", SwingConstants.CENTER);
+	private JTabbedPane theTab = new JTabbedPane(SwingConstants.LEFT);
 	private JMenuBar menuBar = new JMenuBar();
 	private JMenu fileMenu = new JMenu("File");
 	private JMenuItem aboutItem = new JMenuItem("About");
